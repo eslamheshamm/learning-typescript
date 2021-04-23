8e848dd491957daeb53512f470340ce219892e7f
eslamheshamm <esl2mhesham@gmail.com>
2021-04-23 18:09:11 +0200

    learned arrays & union types & objects

--- sandbox.js
+++ sandbox.js
@@ -1,32 +1,51 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from) {
-    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
-        to[j] = from[i];
-    return to;
-};
-var myname = "eslam";
-var age = 22;
-var isFunny = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(4));
+// implicit types
+// let myname = "eslam";
+// let age = 22;
+// let isFunny = true;
+// const circ = (diameter: number) => {
+// 	return diameter * Math.PI;
+// };
+// console.log(circ(4));
+// // arrays
+// let names = ["momen", "hamza", "hima"];
+// names.push("ahmed");
+// console.log(names);
+// let ages = [21, 22, 22];
+// ages.push(24);
+// let info = [...names, ...ages];
+// console.log(info);
+// let person = {
+// 	name: "hamada",
+// 	age: 20,
+// 	favColor: "white",
+// };
+// // person.skills = ['coding','chess']
+// person = {
+// 	name: "eslam",
+// 	age: 22,
+// 	favColor: "red",
+// };
+// console.log(person);
+// explicit types
+var myName;
+var age;
+var isMarried;
 // arrays
-var names = ["momen", "hamza", "hima"];
-names.push("ahmed");
+var names = [];
+names = ["eslam", "hesham"];
 console.log(names);
-var ages = [21, 22, 22];
-ages.push(24);
-var info = __spreadArray(__spreadArray([], names), ages);
-console.log(info);
-var person = {
-    name: "hamada",
-    age: 20,
-    favColor: "white"
-};
-// person.skills = ['coding','chess']
-person = {
-    name: "eslam",
-    age: 22,
-    favColor: "red"
-};
-console.log(person);
+// union types
+var mixed = [];
+mixed.push("hello");
+mixed.push(20);
+// mixed.push(false) doesn't work
+console.log(mixed);
+var uid; // don't have to write () if it's just variable
+uid = "123";
+uid = 123;
+uid;
+// objects
+var person;
+person = { name: "eslam", age: 22 };
+var personTwo;
+personTwo = { name: "momen", age: 22 };
--- sandbox.ts
+++ sandbox.ts
@@ -1,31 +1,65 @@
-let myname = "eslam";
-let age = 22;
-let isFunny = true;
+// implicit types
+// let myname = "eslam";
+// let age = 22;
+// let isFunny = true;
 
-const circ = (diameter: number) => {
-	return diameter * Math.PI;
-};
-console.log(circ(4));
+// const circ = (diameter: number) => {
+// 	return diameter * Math.PI;
+// };
+// console.log(circ(4));
 
+// // arrays
+// let names = ["momen", "hamza", "hima"];
+// names.push("ahmed");
+// console.log(names);
+
+// let ages = [21, 22, 22];
+// ages.push(24);
+// let info = [...names, ...ages];
+// console.log(info);
+
+// let person = {
+// 	name: "hamada",
+// 	age: 20,
+// 	favColor: "white",
+// };
+// // person.skills = ['coding','chess']
+// person = {
+// 	name: "eslam",
+// 	age: 22,
+// 	favColor: "red",
+// };
+// console.log(person);
+
+// explicit types
+
+let myName: string;
+let age: number;
+let isMarried: boolean;
 // arrays
-let names = ["momen", "hamza", "hima"];
-names.push("ahmed");
+let names: string[] = [];
+names = ["eslam", "hesham"];
 console.log(names);
 
-let ages = [21, 22, 22];
-ages.push(24);
-let info = [...names, ...ages];
-console.log(info);
+// union types
 
-let person = {
-	name: "hamada",
-	age: 20,
-	favColor: "white",
+let mixed: (string | number)[] = [];
+mixed.push("hello");
+mixed.push(20);
+// mixed.push(false) doesn't work
+console.log(mixed);
+
+let uid: string | number; // don't have to write () if it's just variable
+uid = "123";
+uid = 123;
+uid;
+
+// objects
+let person: object;
+person = { name: "eslam", age: 22 };
+
+let personTwo: {
+	name: string;
+	age: number;
 };
-// person.skills = ['coding','chess']
-person = {
-	name: "eslam",
-	age: 22,
-	favColor: "red",
-};
-console.log(person);
+personTwo = { name: "momen", age: 22 };
